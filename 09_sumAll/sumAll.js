37302396e0cdc665ac46f66a403d207516505b78
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-06-11 20:12:49 +0300

    solved problem 09 (JS)

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0){
+        return 'ERROR';
+    }
 
+    let arr = [];
+    let i = Math.min(min,max)
+    let j = Math.max(min,max)
+    while(i <= j)
+    {
+        arr.push(i++);
+    }
+    
+    let result = arr.reduce((total, number) => total + number,0)
+    return result;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
