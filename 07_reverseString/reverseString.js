083d4b43a58e37290591154f2a7706eab71ea013
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-06-11 19:05:36 +0300

    solved problem 07 (JS)

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result= new Array(word.length);
+    let j = word.length - 1;
+    for(let i = 0; i < word.length; i++)
+    {   
+        result[i] = word[j--]
+    }
+    return result.join('');
 };
 
 // Do not edit below this line
