e96b86af801b236e6f1ef2deff1f71ec8235cca4
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-06-11 18:18:06 +0300

    solved problem 06 (JS)

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, count) {
 
+    if(count < 0) {return "ERROR";}
+    
+    i = 0;
+    let output = "";
+    while(i < count)
+    {
+        output += word;
+        i++;
+    }
+    return output;
 };
 
 // Do not edit below this line
